5b688b61e886bd441282b7df07b6bd3be228c9f6
BedirT <bedir.tapkan.53@gmail.com>
2016-01-24 14:36:12 -0600

    23rdTutAdded

--- theNewBoston-Study/src/TheNewBostonStudy.java
+++ theNewBoston-Study/src/TheNewBostonStudy.java
@@ -60,6 +60,18 @@ class JavaLearning {
 //        
 
         //22nd tutorial
+        //I already have experience on programming in C; and the syntax is same
+        //so I'll pass this
+        
+        //23rd tutorial
+        double amount, principal=10000, rate=.01 ;
+        
+        for (int day = 1 ; day <= 20 ; day++){
+            amount = principal*Math.pow(1+rate, day);
+            System.out.println(day+ " " + amount);
+                   
+        }
+              
         
     } 
 }
