68fafe553b819957378c3aceaa1f21750232edf9
BedirT <bedir.tapkan.53@gmail.com>
2016-04-15 16:28:48 -0500

    MagicSqr added w/ minor problem

---
+++ SquareTest/src/squaretest/Square.java
@@ -0,0 +1,113 @@
+/*
+should contain a class that represents a square matrix. It should include
+a constructor that gives the size of the square and methods to read values
+into the square, print the square, find the sum of a given row, find the sum
+of a given column, find the sum of the main (or other) diagonal, and determine
+whether the square is magic.
+ */
+package squaretest;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author BedirTapkan
+ */
+public class Square {
+    
+    private int size;
+    int array[][] = new int[1001][1001];
+    Scanner scan = new Scanner(System.in);
+    int sum = 0;
+    
+    Square (int size) {
+        
+        this.size = size;
+        
+    }
+    
+    public void fillMatrix(){
+        
+        for(int i = 0 ; i<size ; i++){
+            for(int j = 0 ; j<size ; j++){
+                array[i][j] = scan.nextInt();
+            }
+        }  
+    }
+    
+    public void printSqr(){
+        
+        for(int i = 0 ; i<size ; i++){
+            for(int j = 0 ; j<size ; j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("");
+        }
+          
+    }
+    
+    private int sumOfRow(int a){
+        
+        sum = 0;
+        for(int i = 0 ; i<size ; i++){
+            sum += array[i][a];
+        }
+        return sum;
+    
+    }
+    
+    private int sumOfColumn(int a){
+        
+        sum = 0;
+        for(int i = 0 ; i<size ; i++){
+            sum += array[a][i];
+        }
+        return sum;
+    
+    }
+    
+    private int sumOfMainDiognal(){
+        
+        sum = 0;
+        for(int i = 0 ; i<size ; i++){
+            sum += array[i][i];
+        }
+        return sum;
+    
+    }
+    
+    private int sumOfOtherDiognal(){
+        
+        sum = 0;
+        for(int i = 0 ; i<size ; i++){
+            sum += array[i][size-i-1];
+        }
+        return sum;
+    }
+    
+    public boolean isMagic(){
+        
+        boolean cond = false;
+        int[] check = new int[2*size+3];
+        for(int i = 0 ; i<size ; i++){
+            check[i] = sumOfRow(i);
+        }
+        for(int i = size ; i<2*size ; i++){
+            check[i] = sumOfColumn(i-size);
+        }
+        check[2*size] = sumOfMainDiognal();
+        check[2*size+1] = sumOfOtherDiognal();
+        
+        for(int i = 0 ; i<2*size+2 ; i++){
+            if(check[i]==check[0]){
+                cond = true;
+            }else{
+                cond = false;
+                break;
+            }
+        }
+        
+        return cond;
+    }
+    
+}
---
+++ SquareTest/src/squaretest/SquareTest.java
@@ -0,0 +1,49 @@
+/*
+SquareTest.java should contain a program that reads input for squares
+from a file named magicData.txt and tells whether each is a magic square.
+Main method should read the size of a square, then after constructing the
+square of that size, it should call the readSquare method to read the square in.
+ */
+package squaretest;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+/**
+ *
+ * @author BedirTapkan
+ */
+public class SquareTest {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        Scanner scan = new Scanner(new File("/Users/BedirTapkan/Desktop/MagicData.txt"));
+        
+        int size = 1;
+        int count = 1;
+        
+        while(size>=0){
+            
+            size = scan.nextInt();
+            scan.next();
+            Square obj1 = new Square(size);
+            obj1.fillMatrix();
+            System.out.println(obj1.isMagic());
+            
+        }
+        
+        
+//        Square obj1 = new Square(3);
+//        
+//        obj1.fillMatrix();
+//        obj1.printSqr();
+//        System.out.println(obj1.isMagic());
+     
+        
+    }
+    
+}
